111e56d28b36fe416fedec6ae9633bb0af9e1b07
Daniel Ramirez <67478253+DRamirezGAAQ@users.noreply.github.com>
2021-11-14 10:46:13 +0300

    Create mario-starter.c

---
+++ mario-starter.c
@@ -0,0 +1,15 @@
+//Feel free to use this code as a starting template to write mario.c
+#include <stdio.h>
+#include <cs50.h>
+
+int main(){
+    int height; //You will need to fill this with user input (ask them to enter something)
+    int spaces; //This will change depending on the behavior in your for loops
+    int hashes; //This will change depending on the behavior in your for loops
+
+    for (int row = 0; row < height; row++){
+
+        //You will need two loops nested inside of this main loop for your program to work
+        //There are multiple ways to solve this problem, using two nested loops is only one of them!
+        //Good Luck!
+}
